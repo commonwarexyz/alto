ff7de182ea309a841f9dc88aa53af1a358f707f0
Patrick O'Grady <me@patrickogrady.xyz>
2025-03-14 14:58:24 -0700

    remove country names

--- explorer/src/App.tsx
+++ explorer/src/App.tsx
@@ -631,7 +631,7 @@ const App: React.FC = () => {
         <div className="map-container">
           <MapContainer center={center} zoom={1} style={{ height: "100%", width: "100%" }} zoomControl={false} scrollWheelZoom={false} doubleClickZoom={false} touchZoom={false} dragging={false}>
             <TileLayer
-              url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+              url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png"
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | &copy; <a href="https://carto.com/attributions">CARTO</a>'
             />
             {views.length > 0 && views[0].location !== undefined && (
