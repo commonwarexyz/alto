7fbe6e04ac620822a96c7d5919404fc97e6864cc
Patrick O'Grady <me@patrickogrady.xyz>
2025-05-22 00:03:22 -0400

    add comments

--- chain/src/actors/application/mod.rs
+++ chain/src/actors/application/mod.rs
@@ -1,4 +1,4 @@
-use alto_types::Identity;
+use alto_types::Evaluation;
 use commonware_cryptography::{
     bls12381::primitives::{group, poly::Poly},
     ed25519::PublicKey,
@@ -16,8 +16,10 @@ pub struct Config {
     /// Participants active in consensus.
     pub participants: Vec<PublicKey>,
 
-    pub polynomial: Poly<Identity>,
+    /// The unevaluated group polynomial associated with the current dealing.
+    pub polynomial: Poly<Evaluation>,
 
+    /// The share of the secret.
     pub share: group::Share,
 
     /// Number of messages from consensus to hold in our backlog
