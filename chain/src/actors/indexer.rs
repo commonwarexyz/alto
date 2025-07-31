c4f95b80580499d0309ee585bdd7874c0bcb1ebe
Patrick O'Grady <me@patrickogrady.xyz>
2025-07-31 10:05:45 -0700

    add seed index

--- chain/src/actors/indexer.rs
+++ chain/src/actors/indexer.rs
@@ -1,16 +1,17 @@
 use alto_types::{Activity, Block};
-use commonware_consensus::{marshal, Reporter};
+use commonware_consensus::{marshal, threshold_simplex::types::Seedable, Reporter, Viewable};
 use commonware_cryptography::bls12381::primitives::variant::MinSig;
-use commonware_runtime::{Handle, Spawner};
+use commonware_runtime::{Metrics, Spawner};
+use tracing::{debug, warn};
 
 #[derive(Clone)]
-pub struct Indexer<E: Spawner, I: crate::Indexer> {
+pub struct Indexer<E: Spawner + Metrics, I: crate::Indexer> {
     context: E,
     indexer: I,
     marshal: marshal::ingress::mailbox::Mailbox<MinSig, Block>,
 }
 
-impl<E: Spawner, I: crate::Indexer> Indexer<E, I> {
+impl<E: Spawner + Metrics, I: crate::Indexer> Indexer<E, I> {
     pub fn new(
         context: E,
         indexer: I,
@@ -24,10 +25,27 @@ impl<E: Spawner, I: crate::Indexer> Indexer<E, I> {
     }
 }
 
-impl<E: Spawner, I: crate::Indexer> Reporter for Indexer<E, I> {
+impl<E: Spawner + Metrics, I: crate::Indexer> Reporter for Indexer<E, I> {
     type Activity = Activity;
 
     async fn report(&mut self, activity: Self::Activity) {
-        unimplemented!()
+        match activity {
+            Activity::Notarization(notarization) => {
+                let view = notarization.view();
+                self.context.with_label("seed").spawn({
+                    let indexer = self.indexer.clone();
+                    let seed = notarization.seed();
+                    move |_| async move {
+                        let result = indexer.seed_upload(seed).await;
+                        if let Err(e) = result {
+                            warn!(?e, "failed to upload seed");
+                            return;
+                        }
+                        debug!(view, "seed uploaded to indexer");
+                    }
+                });
+            }
+            _ => {}
+        }
     }
 }
