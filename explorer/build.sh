18b64e1ca530fed0eb686a4eda956d519a3c426a
Patrick O'Grady <me@patrickogrady.xyz>
2025-08-01 08:38:36 -0700

    use build.sh

---
+++ explorer/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Install Rust if not present
+if ! command -v rustc &> /dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source "$HOME/.cargo/env"
+fi
+
+# Install wasm-pack
+if ! command -v wasm-pack &> /dev/null; then
+    curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+fi
+
+# Run the build
+npm run build
