d3e2633d42b5eb6760103030f2c2c8ca25256cca
Patrick O'Grady <me@patrickogrady.xyz>
2026-07-24 21:34:33 -0700

    update identity

--- explorer/src/global_config.ts
+++ explorer/src/global_config.ts
@@ -1,5 +1,5 @@
 export const BACKEND_URL = "global.alto.exoware.xyz";
-export const PUBLIC_KEY_HEX = "915435a0866d71ada4cd75f33e92c8bc4f31e0b17fbe867c288343b8d770e680240c92aad97d68c2b7144538fc13693209884cd00cbec266e3943feda753ac1db4c6b9756cc682b7bd0aba6ae2cc3aa336a701965cd133b616ca66be445247ff";
+export const PUBLIC_KEY_HEX = "a6ad67a90af5cb7f04015f3df946c0f0f90f3bc3c536cadb3bedbc32eb35de552c2bfce575f69613765b23aaa19524ed06122806decca7be10f1a5709bd77855fc24c20ecb6bdc88320a8526a1f1890704425014a559c2920874f9592faa0c37";
 export const LOCATIONS: [[number, number], string][] = [
     [[37.7749, -122.4194], "San Francisco"],
     [[38.8339, -77.3074], "Ashburn"],
